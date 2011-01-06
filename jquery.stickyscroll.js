9127e8a0e7dec0cd39a1e24991ac4be0185b8d37
Rick Harris <rick.c.harris@gmail.com>
2011-01-06 01:36:42 -0500

    No longer rely on fixed positioning.
    Good for mobile/tablet, but it's flickery in IE

--- jquery.stickyscroll.js
+++ jquery.stickyscroll.js
@@ -25,65 +25,81 @@
 
 (function($) {
 	$.fn.stickyScroll = function(options) {
-		
+	
 		var settings = $.extend({
 				mode: 'auto', // 'auto' or 'manual', but anything other than auto will be treated as manual
 				container: $('body'),
 				bottomBoundary: null
 			}, options);
-			
-		// make sure user input is a jQuery object
-		settings.container = $(settings.container);
-		if(!settings.container.length) {
-			if(console) {
-				console.log('StickyScroll: the element ' + options.container + ' does not exist, we\'re throwing in the towel');
-			}
-			return;
-		}
-		
-		// calculate automatic bottomBoundary
-		if(settings.mode === 'auto') {
-			settings.bottomBoundary = $(document).height() - settings.container.offset().top - settings.container.attr('offsetHeight');
-		}
-		
-		return this.each(function() {
-			
-			var $this = $(this),
-				height = $this.attr('offsetHeight'),
-				topOffset = $this.offset().top,
-				leftOffset = $this.offset().left;
-			
-			function onScroll() {
-				var top = $(document).scrollTop(),
-					bottom = $(document).height() - top - height;
-					
-				if(bottom <= settings.bottomBoundary) {
-					$this.offset({ top: $(document).height() - settings.bottomBoundary - height, left: leftOffset });
-					settings.container.addClass('sticky-processed');
-				}
-				else if(top > topOffset) {
-					$this.css({
-						position: 'fixed',
-						left: leftOffset,
-						top: 0,
-						bottom: ''
-					});
-					settings.container.addClass('sticky-processed');
-				}
-				else if(top < topOffset) {
-					$this.css({
-						position: '',
-						left: '',
-						top: '',
-						bottom: ''
-					});
-					settings.container.removeClass('sticky-processed');
-				}
-			};
-			
-			onScroll();
-			$(window).scroll(onScroll);
 
-		});
+		$.fn.stickyScrollEnable = function() {
+
+			// make sure user input is a jQuery object
+			settings.container = $(settings.container);
+			if(!settings.container.length) {
+				if(console) {
+					console.log('StickyScroll: the element ' + options.container + ' does not exist, we\'re throwing in the towel');
+				}
+				return;
+			}
+
+			// calculate automatic bottomBoundary
+			if(settings.mode === 'auto') {
+				settings.bottomBoundary = $(document).height() - settings.container.offset().top - settings.container.attr('offsetHeight');
+			}
+
+			return this.each(function() {
+
+				var $this = $(this),
+					height = $this.attr('offsetHeight'),
+					topOffset = $this.offset().top;
+
+				function onScroll() {
+					var top = $(document).scrollTop(),
+						bottom = $(document).height() - top - height;
+
+					if(bottom <= settings.bottomBoundary) {
+						$this.offset({ top: $(document).height() - settings.bottomBoundary - height });
+						settings.container.addClass('sticky-processed');
+					}
+					else if(top > topOffset) {
+						$this.offset({ top: $(window).scrollTop() });
+						settings.container.addClass('sticky-processed');
+					}
+					else if(top < topOffset) {
+						$this.css({
+							position: '',
+							top: '',
+							bottom: ''
+						});
+						settings.container.removeClass('sticky-processed');
+					}
+				};
+
+				$(window).bind('scroll.stickyscroll', onScroll).scroll();
+
+			});
+
+		}
+
+		$.fn.stickyScrollReset = function() {
+			this.each(function() {
+				$(this).css({
+					position: '',
+					top: '',
+					bottom: ''
+				});
+				settings.container.removeClass('sticky-processed');
+			});
+			$(window).unbind('.stickyscroll');
+		}
+	
+		if (options === 'reset') {
+			return $(this).stickyScrollReset();
+		}
+		else {
+			return $(this).stickyScrollEnable();
+		}
+	
 	};
 })(jQuery);
